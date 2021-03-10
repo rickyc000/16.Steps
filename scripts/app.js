0569c60e0cb965209d71e0ea0dbe608fffb6cf56
rickyc <r.cato@live.com>
2021-03-10 18:32:18 +0000

    synth set-up, removing samples

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,106 @@
 function init() {
   console.log('Javascript is running')
 
+  const audioContext = new AudioContext()
+  const buffer = audioContext.createBuffer(
+    1, 
+    audioContext.sampleRate * 1,
+    audioContext.sampleRate
+  )
+
+  const channelData = buffer.getChannelData(0)
+  console.log(channelData.length)
+
+  for (let i = 0; i < buffer.length; i++) {
+    channelData[i] = Math.random() * 2 - 1
+  }
+
+
+  const primaryGainControl = audioContext.createGain()
+  primaryGainControl.gain.setValueAtTime(0.05, 0)
+  primaryGainControl.connect(audioContext.destination)
+
+
+  const button = document.createElement('button')
+  button.innerText = 'White Noise'
+  button.addEventListener('click', () => {
+    const whiteNoiseSource = audioContext.createBufferSource()
+    whiteNoiseSource.buffer = buffer
+    whiteNoiseSource.connect(primaryGainControl)
+    whiteNoiseSource.start()
+  })
+  document.body.appendChild(button)
+
+
+  const snareFilter = audioContext.createBiquadFilter()
+  snareFilter.type = 'highpass'
+  snareFilter.frequency.value = 500
+  snareFilter.connect(primaryGainControl)
+
+
+  const snareButton = document.createElement('button')
+  snareButton.innerText = 'Snare'
+  snareButton.addEventListener('click', () => {
+    const whiteNoiseSource = audioContext.createBufferSource()
+    whiteNoiseSource.buffer = buffer
+    whiteNoiseSource.connect(snareFilter)
+    whiteNoiseSource.start()
+  })
+
+  document.body.appendChild(snareButton)
+
+
+
+
+
+  function playSynth(freq) {
+    const synthOscillator = audioContext.createOscillator()
+    // kickOscillator.frequency.exponentialRampToValueAtTime(
+    //   0.001,
+    //   audioContext.currentTime + 0.4
+    // )
+    synthOscillator.frequency.setValueAtTime(freq, 0)
+    synthOscillator.type = 'sawtooth'
+    synthOscillator.connect(snareFilter)
+    synthOscillator.start()
+    synthOscillator.stop(audioContext.currentTime + 0.6)
+  }
+
+
   let timerId = null
   let playheadPosition = 0
   let isPlaying = false
 
-  //* 909 samples
-  const samples = [
-    '',
-    '../assets/sounds/909kit_BD2.mp3',
-    '../assets/sounds/909kit_ClapST.mp3',
-    '../assets/sounds/909kit_Crash.mp3',
-    '../assets/sounds/909kit_HH1.mp3',
-    '../assets/sounds/909kit_HHo2.mp3',
-    '../assets/sounds/909kit_Ride1.mp3',
-    '../assets/sounds/909kit_RideST.mp3',
-    '../assets/sounds/909kit_Rim1.mp3',
-    '../assets/sounds/909kit_SD1.mp3',
-    '../assets/sounds/909kit_Tom4.mp3'
-  ]
+  // //* 909 samples
+  // const samples = [
+  //   '',
+  //   '../assets/sounds/909kit_BD2.mp3',
+  //   '../assets/sounds/909kit_ClapST.mp3',
+  //   '../assets/sounds/909kit_Crash.mp3',
+  //   '../assets/sounds/909kit_HH1.mp3',
+  //   '../assets/sounds/909kit_HHo2.mp3',
+  //   '../assets/sounds/909kit_Ride1.mp3',
+  //   '../assets/sounds/909kit_RideST.mp3',
+  //   '../assets/sounds/909kit_Rim1.mp3',
+  //   '../assets/sounds/909kit_SD1.mp3',
+  //   '../assets/sounds/909kit_Tom4.mp3'
+  // ]
 
+  //* Notes
+  const notes = [
+    { name: 'C', frequency: 261.63 },
+    { name: 'C#', frequency: 277.18 },
+    { name: 'D', frequency: 293.66 },
+    { name: 'D#', frequency: 311.13 },
+    { name: 'E', frequency: 329.63 },
+    { name: 'F', frequency: 349.23 },
+    { name: 'F#', frequency: 369.99 },
+    { name: 'G', frequency: 392.0 },
+    { name: 'G#', frequency: 415.3 },
+    { name: 'A', frequency: 440.0 },
+    { name: 'A#', frequency: 466.16 },
+    { name: 'B', frequency: 493.88 }
+  ]
 
   //* Grid variables
   const grid = document.querySelector('.grid')
@@ -53,20 +134,20 @@ function init() {
   createChannels()
 
   //* Selecting the audio tags
-  const audioTags = []
-  for (let i = 1; i <= channels; i++) {
-    audioTags[i] = document.querySelector(`.channel${i}`)
-  }
-  // console.log(audioTags)
+  // const audioTags = []
+  // for (let i = 1; i <= channels; i++) {
+  //   audioTags[i] = document.querySelector(`.channel${i}`)
+  // }
 
 
-  function handlePlaySound(channel) {
-    // console.log(event)
-    const sound = samples[channel]
-    // console.log(sound)
-    audioTags[channel].src = sound
-    audioTags[channel].play()
-  }
+
+  // function handlePlaySound(channel) {
+  //   // console.log(event)
+  //   const sound = samples[channel]
+  //   // console.log(sound)
+  //   audioTags[channel].src = sound
+  //   audioTags[channel].play()
+  // }
 
   //* Controls 
   const playButton = document.querySelector('.play-button')
@@ -79,6 +160,7 @@ function init() {
       console.log('play!')
       playheadPosition = 1
       updateCells()
+      triggersSample()
       startTimer()
       isPlaying = true
     } else {
@@ -98,16 +180,15 @@ function init() {
   //* Clear grid
   function handleClearGrid() {
     console.log('clear')
-    handlePlaySound(5)
   }
 
 
   //* Timer
   function startTimer() {
     timerId = setInterval(() => {
-      // console.log(timerId)
+      console.log(timerId)
       movePlayhead()
-    }, 250)
+    }, 150)
   }
 
 
@@ -151,9 +232,10 @@ function init() {
     for (let i = 0; i < cells.length; i++) {
       if (cells[i].classList.contains(`X${playheadPosition}`)
         && (cells[i].classList.contains('on'))) {
-        const sampleToPlay = cells[i].classList[0]
-        // console.log(sampleToPlay.slice(1))
-        handlePlaySound(sampleToPlay.slice(1))
+        const noteToPlay = cells[i].classList[0].slice(1)
+        // console.log(noteToPlay.slice(1))
+        // handlePlaySound(noteToPlay)
+        playSynth(notes[noteToPlay].frequency)
       }
     }
   }
